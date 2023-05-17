63525da129b509df4d29590483fe3183b4f79a6e
alberico fagundes <alberico@fagundes.live>
2023-05-17 14:43:59 -0300

    primeiro teste conexao

--- w3-bet-back/index.js
+++ w3-bet-back/index.js
@@ -1,9 +1,24 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "seu_usuario",
+  user: "postgres",
   host: "localhost",
-  database: "seu_banco_de_dados",
-  password: "sua_senha",
-  port: 5432,
+  database: "db_w3bets",
+  password: "99411520",
+  port: 5433,
+  connectionTimeoutMillis: 9000, // Aumente esse valor conforme necessário
 });
+
+// Função para realizar a conexão e enviar mensagem de sucesso ou erro
+async function conectarAoBancoDeDados() {
+  try {
+    const client = await pool.connect();
+    console.log("Conexão bem-sucedida com o banco de dados!");
+    client.release(); // Liberar o cliente de volta para o pool
+  } catch (error) {
+    console.error("Erro ao conectar ao banco de dados:", error);
+  }
+}
+
+// Chamar a função para conectar ao banco de dados
+conectarAoBancoDeDados();
